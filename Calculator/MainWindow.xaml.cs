4fa910e0f89238449ebda1d65e57838496df4563
VasulS <putzalin@gmail.com>
2016-08-07 00:07:44 +0300

    Button and checkbox handling

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -38,39 +38,63 @@ namespace Calculator
             //s = TextBox1.Text;
             //strEval = new StringEvaluater(s);
             //label1.Content = TextBox1.Text + " = " + strEval.Eval();
-            num1 = strEval.Operation(num1, operation, TextBox1.Text);
-            label1.Content += " " + TextBox1.Text + " = " + num1;
-            TextBox1.Text = num1;
-            number_click = false;
-            operation_click = false;
+            if ((bool)checkBox1.IsChecked)
+            {
+                strEval = new StringEvaluater(TextBox1.Text);
+                label1.Content += TextBox1.Text;
+                TextBox1.Text = strEval.Eval();
+                label1.Content += " = " + TextBox1.Text;
+            }
+            else
+            {
+                num1 = strEval.Operation(num1, operation, TextBox1.Text);
+                label1.Content += " " + TextBox1.Text + " = " + num1;
+                TextBox1.Text = num1;
+                number_click = false;
+                operation_click = false;
+            }       
         }
 
         private void Button_Click_Number(object sender, RoutedEventArgs e)
         {
-            //string operation = (string)((Button)e.OriginalSource).Content;
-            if (operation_click)
+            if ((bool)checkBox1.IsChecked)
             {
-                TextBox1.Text = "";
-                operation_click = false;
+                TextBox1.Text += (string)((Button)e.OriginalSource).Content;
             }
-            TextBox1.Text += (string)((Button)e.OriginalSource).Content;
+            else
+            {
+                //string operation = (string)((Button)e.OriginalSource).Content;
+                if (operation_click)
+                {
+                    TextBox1.Text = "";
+                    operation_click = false;
+                }
+                TextBox1.Text += (string)((Button)e.OriginalSource).Content;
+            }
         }
 
         private void Button_Click_Operation(object sender, RoutedEventArgs e)
         {
-            operation = (string)((Button)e.OriginalSource).Content;
-            if (number_click)
+            if ((bool)checkBox1.IsChecked)
             {
-                label1.Content += " " + TextBox1.Text + " " + " " + operation;
-                TextBox1.Text = num1 = strEval.Operation(num1, operation, TextBox1.Text);                
+                TextBox1.Text += (string)((Button)e.OriginalSource).Content;
             }
             else
             {
-                num1 = TextBox1.Text;
-                label1.Content += TextBox1.Text + " " + " " + operation;
-                number_click = true;
+                operation = (string)((Button)e.OriginalSource).Content;
+                if (number_click)
+                {
+                    label1.Content += " " + TextBox1.Text + " " + operation;
+                    TextBox1.Text = num1 = strEval.Operation(num1, operation, TextBox1.Text);
+                }
+                else
+                {
+                    num1 = TextBox1.Text;
+                    label1.Content += TextBox1.Text + " " + operation;
+                    number_click = true;
+                }
+                operation_click = true;
             }
-            operation_click = true;
         }
 
         private void PreviewKeyDownHandler(object sender, KeyEventArgs e)
